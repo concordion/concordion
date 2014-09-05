6f260bf9e877c811a4476372a731ad87b605dc69
jacek-ratzinger <jacek.ratzinger@gmx.at>
2014-09-05 20:19:11 +0200

    use all Concordion commands as lower case

---
+++ src/test/java/spec/concordion/command/CaseInsensitiveCommandsTest.java
@@ -0,0 +1,16 @@
+﻿package spec.concordion.command;
+
+import test.concordion.TestRig;
+
+@RunWith(ConcordionRunner.class)
+public class CaseInsensitiveCommandsTest {
+	
+	public String process(String snippet, Object stubbedResult) throws Exception {
+		long successCount = new TestRig()
+			.withStubbedEvaluationResult(stubbedResult)
+			.processFragment(snippet)
+			.getSuccessCount();
+		
+		return successCount == 1 ? snippet : "Did not work";
+	}
+}
