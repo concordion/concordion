43d6e2607cf7ccb27c7e490d9debe200a9c3e61d
jacek-ratzinger <jacek.ratzinger@gmx.at>
2014-09-05 21:11:21 +0200

    fix spec tests

--- src/test/java/spec/concordion/command/CaseInsensitiveCommandsTest.java
+++ src/test/java/spec/concordion/command/CaseInsensitiveCommandsTest.java
@@ -7,6 +7,18 @@ import org.junit.runner.RunWith;
 @RunWith(ConcordionRunner.class)
 public class CaseInsensitiveCommandsTest {
 	
+	public String processEquals(String snippet) throws Exception {
+		return process(snippet, "Fred");
+	}
+	
+	public String processTrue(String snippet) throws Exception {
+		return process(snippet, true);
+	}
+	
+	public String processFalse(String snippet) throws Exception {
+		return process(snippet, false);
+	}
+	
 	public String process(String snippet, Object stubbedResult) throws Exception {
 		long successCount = new TestRig()
 			.withStubbedEvaluationResult(stubbedResult)
