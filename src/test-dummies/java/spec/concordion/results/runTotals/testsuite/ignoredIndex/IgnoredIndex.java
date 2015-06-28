ee75c8a46cfeef9697746d8806bd63e96af03415
Tim Wright <tim@tfwight.co.nz>
2015-06-29 07:40:38 +1200

    Added Nigel's run suite into the test-dummies folder

---
+++ src/test-dummies/java/spec/concordion/results/runTotals/testsuite/ignoredIndex/IgnoredIndex.java
@@ -0,0 +1,8 @@
+package spec.concordion.results.runTotals.testsuite.ignoredIndex;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class IgnoredIndex {
+}
---
+++ src/test-dummies/java/spec/concordion/results/runTotals/testsuite/invalidHtml/InvalidHtml.java
@@ -0,0 +1,8 @@
+package spec.concordion.results.runTotals.testsuite.invalidHtml;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class InvalidHtml {
+}
