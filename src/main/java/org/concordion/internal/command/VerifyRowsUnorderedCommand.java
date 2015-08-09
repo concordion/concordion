b24ca419bc1eee3b91b5e8134e7995b07018f256
Ievgen Degtiarenko <gmandnepr@gmail.com>
2015-08-09 11:40:57 -0400

    Supress compilation warning

--- src/main/java/org/concordion/internal/command/VerifyRowsUnorderedCommand.java
+++ src/main/java/org/concordion/internal/command/VerifyRowsUnorderedCommand.java
@@ -14,6 +14,7 @@ import java.util.regex.Pattern;
 
 public class VerifyRowsUnorderedCommand extends AbstractVerifyRowsCommand {
 
+    @SuppressWarnings("unchecked")
     @Override
     public void verify(CommandCall commandCall, Evaluator evaluator, ResultRecorder resultRecorder) {
         Pattern pattern = Pattern.compile("(#.+?) *: *(.+)");
