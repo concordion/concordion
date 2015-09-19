b01791be9065d8101292a989629692db8daf2e29
Ievgen Degtiarenko <gmandnepr@gmail.com>
2015-09-18 22:57:19 -0400

    Minimize examples in the rest of the specs

--- src/test/java/spec/concordion/command/verifyRows/strategies/BaseMatchStrategyTest.java
+++ src/test/java/spec/concordion/command/verifyRows/strategies/BaseMatchStrategyTest.java
@@ -13,6 +13,27 @@ public class BaseMatchStrategyTest {
 
     public List<MultiValueResult> users;
 
+    private static final String ROWS_PLACEHOLDER = "\n   [ROWS]\n";
+
+    public String processRows(String template, String expectedRows, String actualData) throws Exception {
+        return unwrapRows(template,
+                processFragment(
+                        wrapRows(template, expectedRows),
+                        actualData
+                )
+        );
+    }
+
+    private String wrapRows(String template, String fragment) {
+        return template.replace(ROWS_PLACEHOLDER, fragment);
+    }
+
+    private String unwrapRows(String template, String actualTable) {
+        String tableStart = template.substring(0, template.indexOf(ROWS_PLACEHOLDER));
+        String tableEnd = template.substring(template.indexOf(ROWS_PLACEHOLDER) + ROWS_PLACEHOLDER.length());
+        return actualTable.replace(tableStart, "").replace(tableEnd, "");
+    }
+
     public String processFragment(String fragment, String actualData) throws Exception {
         users = parse(actualData);
 
