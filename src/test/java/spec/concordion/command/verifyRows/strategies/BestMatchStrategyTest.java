bf95ae58c0ad34d9d66022b85a2d2f82c192a6d9
Nigel Charman <nigel.charman.nz@gmail.com>
2015-09-20 22:02:27 +1200

    Enhanced verify-rows strategy specifications: * new Strategies.html specification * added toggling of Rendered and HTML views of examples * some changes to wording and examples of existing specifications

--- src/test/java/spec/concordion/command/verifyRows/strategies/BestMatchStrategyTest.java
+++ src/test/java/spec/concordion/command/verifyRows/strategies/BestMatchStrategyTest.java
@@ -3,8 +3,6 @@ package spec.concordion.command.verifyRows.strategies;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
-
 @RunWith(ConcordionRunner.class)
 public class BestMatchStrategyTest extends BaseMatchStrategyTest {
-
 }
