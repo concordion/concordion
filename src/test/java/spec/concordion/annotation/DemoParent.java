3aa90c85970a6dc033a6f4ceff14aab5a2c82356
Andrew Sumner <andrew.sumner@assurity.co.nz>
2015-10-05 17:36:26 +1300

    Tidy demo

--- src/test/java/spec/concordion/annotation/DemoParent.java
+++ src/test/java/spec/concordion/annotation/DemoParent.java
@@ -6,7 +6,7 @@ import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
 @RunWith(ConcordionRunner.class)
-@CopyResource(sourceFiles = "/concordion.css", removeDefaultCSS = false, insertType = InsertType.EMBEDDED)
+@CopyResource(sourceFiles = "/concordion.css", removeDefaultCSS = true, insertType = InsertType.LINKED)
 public class DemoParent {
     
 }
--- src/test/java/spec/concordion/annotation/DemoTest.java
+++ src/test/java/spec/concordion/annotation/DemoTest.java
@@ -1,11 +1,12 @@
 package spec.concordion.annotation;
 
-import org.concordion.integration.junit4.ConcordionRunner;
-import org.junit.runner.RunWith;
+import org.concordion.api.CopyResource;
+import org.concordion.api.CopyResource.InsertType;
 
-@RunWith(ConcordionRunner.class)
-public class DemoTest {
-
+//@RunWith(ConcordionRunner.class)
+@CopyResource(sourceFiles = { "*.css", "demo.txt" }, insertType = InsertType.LINKED)
+public class DemoTest extends DemoParent {
+    
     public String greetingFor(String firstName) {
         return String.format("Hello %s!", firstName);
     }
