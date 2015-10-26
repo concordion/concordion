ff132c09cdb605814d2e3219a4bc52e55921853f
Andrew Sumner <andrew.sumner@assurity.co.nz>
2015-10-27 11:48:29 +1300

    Remove TODO

--- src/test/java/spec/concordion/annotation/DemoParent.java
+++ src/test/java/spec/concordion/annotation/DemoParent.java
@@ -4,7 +4,6 @@ import org.concordion.api.Resources;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
-//TODO Delete me when happy with Resources annotation
 @RunWith(ConcordionRunner.class)
 @Resources(value = "/concordion.css", includeDefaultStyling = false)
 public class DemoParent {
