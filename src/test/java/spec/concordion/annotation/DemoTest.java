9c8015cce0c19c22421392be8effe7c880426a49
Andrew Sumner <andrew.sumner@assurity.co.nz>
2015-10-27 11:45:06 +1300

    Tidy demo

--- src/test/java/spec/concordion/annotation/DemoTest.java
+++ src/test/java/spec/concordion/annotation/DemoTest.java
@@ -1,11 +1,14 @@
 package spec.concordion.annotation;
 
+import org.concordion.api.Resources;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
+//TODO Delete me and other Demo files when happy with Resources annotation
 @RunWith(ConcordionRunner.class)
+@Resources(value = {"demo.css", "demo.js", "demo.txt"})
 public class DemoTest {
-
+	
     public String greetingFor(String firstName) {
         return String.format("Hello %s!", firstName);
     }
