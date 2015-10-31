6b57d8d0cd823650efc47a210954b2145cb06c73
Nigel Charman <nigel.charman.nz@gmail.com>
2015-10-31 18:49:28 +1300

    Added an example for accessing a resource from a fixture in a package

--- src/test/java/spec/concordion/annotation/ResourcesTest.java
+++ src/test/java/spec/concordion/annotation/ResourcesTest.java
@@ -2,11 +2,9 @@ package spec.concordion.annotation;
 
 import java.io.File;
 import java.net.URISyntaxException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import org.concordion.api.Resource;
 import org.concordion.api.Element;
+import org.concordion.api.Resource;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
@@ -16,9 +14,8 @@ import test.concordion.TestRig;
 
 @RunWith(ConcordionRunner.class)
 public class ResourcesTest {
-	private TestRig testRig = null;
+    private TestRig testRig = null;
 	private JavaSourceCompiler compiler;
-	private static final Pattern CLASS_NAME_PATTERN = Pattern.compile("class\\s*(.*?)\\s*(\\{|extends)");
 	private ProcessingResult result;
 	
     public void process(String javaFragment) throws Exception {
@@ -48,11 +45,11 @@ public class ResourcesTest {
     }
     
     private Object compile(String javaSource) throws Exception, InstantiationException, IllegalAccessException {
-    	return compiler.compile(getClassName(javaSource), javaSource).newInstance();
+        return compiler.compile(javaSource).newInstance();
 	}
     
     private Object compile(String javaSource, String javaSourceParent) throws Exception, InstantiationException, IllegalAccessException {
-    	return compiler.compile(getClassName(javaSource), javaSource, getClassName(javaSourceParent), javaSourceParent).newInstance();
+        return compiler.compileWithParent(javaSource, javaSourceParent).newInstance();
 	}
 
 	private ProcessingResult process(String htmlFragment, Object fixture) {
@@ -62,32 +59,12 @@ public class ResourcesTest {
             .withResource(new Resource("/demo.css"), "")
             .withResource(new Resource("/spec/demo.js"), "")
             .withResource(new Resource("/spec/demo.txt"), "")
+            .withResource(new Resource("/resources/test/demo.css"), "")
             .processFragment(htmlFragment);
         
         return result;
     }
 	
-    private String getClassName(String javaFragment) {
-        Matcher matcher = CLASS_NAME_PATTERN.matcher(javaFragment);
-        matcher.find();
-        
-        return matcher.group(1);
-    }
-    
-//    private String getPackageName(String javaFragment) {
-//    	Matcher matcher = Pattern.compile("(?m)^package\\s*(.*);$").matcher(javaFragment);
-//        //Matcher matcher = PACKAGE_NAME_PATTERN.matcher(javaFragment);
-//        matcher.find();
-//        
-//        String packageName = matcher.group(1);
-//        
-//        if (!packageName.isEmpty()) {
-//        	packageName += ".";
-//        }
-//        
-//        return packageName;
-//    }
-    
     public String getLink(String expectedResource) {
     	Element[] links = result.getRootElement().getFirstChildElement("head").getChildElements("link");
     	
