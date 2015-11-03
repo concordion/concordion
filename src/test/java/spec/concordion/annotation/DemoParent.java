523f33850747a41d2238fe0c1627fd056b4ee0db
Andrew Sumner <andrew.sumner@assurity.co.nz>
2015-11-03 15:56:06 +1300

    Stop gradle trying to run this

--- src/test/java/spec/concordion/annotation/DemoParent.java
+++ src/test/java/spec/concordion/annotation/DemoParent.java
@@ -4,7 +4,6 @@ import org.concordion.api.Resources;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
-@RunWith(ConcordionRunner.class)
 @Resources(value = { "/concordion.css" }, includeDefaultStyling = false)
 public class DemoParent {
     
