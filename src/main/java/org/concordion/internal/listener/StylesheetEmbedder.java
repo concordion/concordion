0ee727cbbf56f7d74252b416d2aa84e5d95ac62d
Nigel Charman <nigel.charman.nz@gmail.com>
2015-11-15 15:22:35 +1300

    Added example styling to concordion:example command. You no longer need to add a class=example attribute. An updated copy of concordion.css is available in the src/test/resources folder, which adds the example styling to elements containing the example command (note that this css file assumes that your concordion namespace prefix is set to concordion or c). The stylesheet embedder will dynamically discover the concordion namespace prefix and apply the correct prefix to the embedded CSS even if you have named your concordion namespace prefix to something other than concordion or c.

--- src/main/java/org/concordion/internal/listener/StylesheetEmbedder.java
+++ src/main/java/org/concordion/internal/listener/StylesheetEmbedder.java
@@ -4,6 +4,7 @@ import nu.xom.Document;
 import nu.xom.Element;
 
 import org.concordion.api.listener.DocumentParsingListener;
+import org.concordion.internal.ConcordionBuilder;
 import org.concordion.internal.util.Check;
 
 public class StylesheetEmbedder implements DocumentParsingListener {
@@ -26,12 +27,32 @@ public class StylesheetEmbedder implements DocumentParsingListener {
         Element head = html.getFirstChildElement("head");
         Check.notNull(head, "<head> section is missing from document");
         Element style = new Element("style");
-        style.appendChild(stylesheetContent);
-        
+
+        String updatedStylesheetContent = updateConcordionNamespacePrefix(html, stylesheetContent);
+        style.appendChild(updatedStylesheetContent);
+
         if (appendChild) {
         	head.appendChild(style);
         } else {
         	head.insertChild(style, 0);
         }
+    }
+
+    /**
+     * If the prefix for the Concordion namespace is something other than "concordion", eg "c", this 
+     * updates the stylesheet to use the prefix.
+     * This is required since <a href="http://stackoverflow.com/questions/24628932/do-css-namespace-attribute-selectors-work-with-xhtml-html-elements">
+     * namespaced CSS selectors generally don't work with HTML</a>, so <code>embedded.css</code> uses hardcoded namespace prefixes (eg. <code>[concordion\:example]</code>).
+     */
+    private String updateConcordionNamespacePrefix(Element html, String stylesheetContent) {
+        for (int i=0; i<html.getNamespaceDeclarationCount(); i++) {
+            String prefix = html.getNamespacePrefix(i);
+            System.out.println(prefix);
+            System.out.println(html.getNamespaceURI(prefix));
+            if (ConcordionBuilder.NAMESPACE_CONCORDION_2007.equals(html.getNamespaceURI(prefix))) {
+                return stylesheetContent.replace("concordion\\:", prefix + "\\:");
+            }
+        }
+        return stylesheetContent;
     }
 }
