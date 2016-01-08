40868919860bfe27b89f82bb0f9135a1a595902e
Nigel Charman <nigel.charman.nz@gmail.com>
2016-01-09 09:59:07 +1300

    Made lazy initialisation thread-safe in Scoped class

--- src/main/java/org/concordion/api/Scoped.java
+++ src/main/java/org/concordion/api/Scoped.java
@@ -2,17 +2,29 @@ package org.concordion.api;
 
 public abstract class Scoped<T> {
 
-    private T value;
+    private volatile T value;
 
     protected abstract T create();
 
     protected void destroy(T t) {
     }
 
+    /**
+     * Return the value of the variable. 
+     * Uses lazy initialisation to call the {@link #create()} method (implemented safely for Java 5.0+, see
+     * http://www.oracle.com/technetwork/articles/javase/bloch-effective-08-qa-140880.html).
+     * @return value of scoped variable
+     */
     public T get() {
-        if (value == null) {
-            value = create();
+        T result = value;
+        if (result == null) { // First check (no locking)
+            synchronized (this) {
+                result = value;
+                if (result == null) { // Second check (with locking)
+                    value = result = create();
+                }
+            }
         }
-        return value;
+        return result;
     }
 }
