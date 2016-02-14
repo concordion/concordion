d9cf69dff3f34399d107ca03fb6d6fc8b4ce4ad7
Nigel Charman <nigel.charman.nz@gmail.com>
2016-02-14 13:56:17 +1300

    Add check for old ExcelExtension which needs updating due to breaking changes.
    Source and SpecificaitonLocator interfaces have breaking changes, and the Excel Extension has been updated to use the new
    withSpecificationType() extension method instead.

--- src/main/java/org/concordion/internal/extension/ExtensionChecker.java
+++ src/main/java/org/concordion/internal/extension/ExtensionChecker.java
@@ -7,9 +7,11 @@ public class ExtensionChecker {
 
     private static final String MARKDOWN_EXTENSION_CLASS = "org.concordion.ext.MarkdownExtension";
     private static final String EXTENSIONS_CLASS = "org.concordion.ext.Extensions";
+    private static final String EXCEL_EXTENSION_SOURCE_CLASS = "org.concordion.ext.excel.ExcelClassPathSource";
 
     public static void checkForOutdatedExtensions() {
         checkForExtensionsNowInCore();
+        checkForBreakingChanges();
         checkForDeprecatedExtensions();
     }
 
@@ -20,6 +22,16 @@ public class ExtensionChecker {
             return; // We don't want it to be found
         }
         String msg = "The Markdown format is now supported in the main Concordion module. The concordion-markdown-extension module must be removed from the class path.";
+        throw new ConfigurationException(msg);
+    }
+
+    private static void checkForBreakingChanges() {
+        try {
+            Class.forName(EXCEL_EXTENSION_SOURCE_CLASS);
+        } catch (ClassNotFoundException expected) {
+            return; // We don't want it to be found
+        }
+        String msg = "The Concordion Excel Extension must be updated to the latest version to work with Concordion 2.0 or later.";
         throw new ConfigurationException(msg);
     }
 
