bb7badd5b4bcdd0b81cd7b5647025221cf607208
Nigel Charman <nigel.charman.nz@gmail.com>
2016-03-05 09:59:17 +1300

    Added spec for fail-fast on expected-to-fail example.
    Issue #144 is already fixed, this bakes in the functionality.
    Also fixed typo in FailFastPassthrough spec.

---
+++ src/test/java/spec/concordion/command/example/SpecCallingFailFastExamplesFixture.java
@@ -0,0 +1,10 @@
+package spec.concordion.command.example;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+import spec.concordion.results.runTotals.RunTotalsFixture;
+
+@RunWith(ConcordionRunner.class)
+public class SpecCallingFailFastExamplesFixture extends RunTotalsFixture {
+}
