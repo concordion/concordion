661a2496cf2e126bc4af48b194543a7d9c6cbc54
Nigel Charman <nigel.charman.nz@gmail.com>
2016-03-05 15:52:43 +1300

    Refactored code to reduce number of ResultSummary classes created

--- src/main/java/org/concordion/internal/cache/ConcordionRunOutput.java
+++ src/main/java/org/concordion/internal/cache/ConcordionRunOutput.java
@@ -3,24 +3,22 @@ package org.concordion.internal.cache;
 import org.concordion.api.FixtureDeclarations;
 import org.concordion.api.Result;
 import org.concordion.api.ResultSummary;
-import org.concordion.internal.SingleResultSummary;
 
 /**
  * Data to store in the cache
  */
 public class ConcordionRunOutput {
+    private static final CacheResultSummary IN_PROGRESS_RESULT_SUMMARY = new CacheResultSummary(Result.IGNORED,
+            "No current results as the specification is currently being executed");
     private ResultSummary actualResultSummary;
     private ResultSummary modifiedResultSummary;
 
     public ConcordionRunOutput(FixtureDeclarations fixture) {
-        SingleResultSummary singleResultSummary = new CacheResultSummary(Result.IGNORED,
-                "No current results for fixture " + fixture + " as the specification is currently being executed");
-        this.actualResultSummary = singleResultSummary;
-        this.modifiedResultSummary = singleResultSummary;
+        this.actualResultSummary = IN_PROGRESS_RESULT_SUMMARY;
+        this.modifiedResultSummary = IN_PROGRESS_RESULT_SUMMARY;
     }
 
     public ConcordionRunOutput(ResultSummary actualResultSummary, ResultSummary modifiedResultSummary) {
-        super();
         this.actualResultSummary = actualResultSummary;
         this.modifiedResultSummary = modifiedResultSummary;
     }
