451608d734ee36e0af869e03b6ea70a60d561c56
Nigel Charman <nigel.charman.nz@gmail.com>
2016-03-13 13:18:33 +1300

    Documented inline HTML for Markdown specs

--- src/test/java/spec/concordion/specificationType/markdown/MarkdownFixture.java
+++ src/test/java/spec/concordion/specificationType/markdown/MarkdownFixture.java
@@ -7,12 +7,4 @@ import org.junit.runner.RunWith;
 @RunWith(ConcordionRunner.class)
 @ConcordionResources({"img/*","Markdown.css"})
 public class MarkdownFixture {
-    
-    public void parseNode(String text, int level) {
-        System.out.println(text + " : " + level);
-    }
-    
-    public String greetingFor(String firstName) {
-        return "Hello Bob!";
-    }
 }
