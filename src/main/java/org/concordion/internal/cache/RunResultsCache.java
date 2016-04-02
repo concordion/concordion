concordion/concordion

commit 5cd0cc7cc852952332b619652800af4f45d18954

Fix aggregation of totals for ParallelRunExtension.
The ParallelRunStrategy creates a ListenableFuture<ResultSummary>. Due to the RunResultsCache creating new ResultRecorders, the results that were being updated by the RunStrategy were no longer being used.

The "Outer Example" results were also being printed before the specification had finished processing. Since the parallel run extension uses the afterProcessingSpecification event to wait for completion, the results need to be printed by the ConcordionRunner.

Fixes #156.

src/main/java/org/concordion/internal/cache/RunResultsCache.java
@@ -61,7 +61,6 @@ public enum RunResultsCache {
     public synchronized ConcordionRunOutput startRun(Fixture fixture, String example) {
         assert fixture.getFixtureClass() != null;
 
-
         // check to see if there is a result in the cache
         ConcordionRunOutput runSummary = getExampleFromCache(fixture, example);
         if (runSummary != null) {
@@ -86,67 +85,67 @@ public enum RunResultsCache {
      * Updates the cache with the results of a run.
      * @param fixture the fixture to update
      * @param example the name of the example that is being finished (null ok)
-     * @param actualSummary the results as reported from the spec
+     * @param actualResultSummary the results as reported from the spec
      * @param modifiedResultSummary the results as post processed by any fixture annotations
      */
     public synchronized void finishRun(Fixture fixture,
                                        String example,
-                                       ResultSummary actualSummary,
+                                       ResultSummary actualResultSummary,
                                        ResultSummary modifiedResultSummary) {
         assert fixture.getFixtureClass() != null;
-        assert actualSummary != null;
+        assert actualResultSummary != null;
         assert modifiedResultSummary != null;
 
+        setResultsForExample(fixture, example, actualResultSummary, modifiedResultSummary);
+
+        addResultsToFixtureTotal(fixture, actualResultSummary, modifiedResultSummary);
+    }
+
+    private void setResultsForExample(Fixture fixture, String example, ResultSummary actualResultSummary, ResultSummary modifiedResultSummary) {
         ConcordionRunOutput exampleResults = getExampleFromCache(fixture, example);
         if (exampleResults == null) {
             throw new IllegalStateException("Internal error: startRun must always be called before finishRun");
         }
-        
-        exampleResults.setActualResultSummary(actualSummary);
+        if (isOuterExample(example)) {
+            // Clone since the outer example is also used for the total result summary
+            actualResultSummary = clone(actualResultSummary);
+            modifiedResultSummary = clone(modifiedResultSummary);
+        }
+        exampleResults.setActualResultSummary(actualResultSummary);
         exampleResults.setModifiedResultSummary(modifiedResultSummary);
-
-        // now accumulate into the parent
-        ConcordionRunOutput parentResults = getParentFromCacheOrCreate(fixture, actualSummary.getSpecificationDescription()); 
-        ResultSummary totalActualResults = addResults(parentResults.getActualResultSummary(), actualSummary);
-        parentResults.setActualResultSummary(totalActualResults);
-        ResultSummary totalModifiedResults = addResults(parentResults.getModifiedResultSummary(), modifiedResultSummary);
-        parentResults.setModifiedResultSummary(totalModifiedResults);
     }
 
-    private ResultSummary addResults(ResultSummary accumulator, ResultSummary resultsToAdd) {
-        SummarizingResultRecorder recorder;
-        if (accumulator instanceof SummarizingResultRecorder) {
-            recorder = (SummarizingResultRecorder) accumulator; 
+    private boolean isOuterExample(String example) {
+        return XMLSpecification.OUTER_EXAMPLE_NAME.equals(example);
+    }
+
+    private void addResultsToFixtureTotal(Fixture fixture, ResultSummary actualResultSummary, ResultSummary modifiedResultSummary) {
+        ConcordionRunOutput fixtureTotalResults = map.get(getID(fixture, null));
+        if (fixtureTotalResults == null) {
+            if (actualResultSummary == modifiedResultSummary) {
+                // Clone so that we don't add the results twice to the same summary
+                modifiedResultSummary = clone(modifiedResultSummary);  
+            }
+            fixtureTotalResults = new ConcordionRunOutput(actualResultSummary, modifiedResultSummary);
+            map.put(getID(fixture, null), fixtureTotalResults);
         } else {
-            recorder = new SummarizingResultRecorder();
-            recorder.record(accumulator);
+            addResults((SummarizingResultRecorder) fixtureTotalResults.getActualResultSummary(), actualResultSummary);
+            addResults((SummarizingResultRecorder) fixtureTotalResults.getModifiedResultSummary(), modifiedResultSummary);
         }
-        
+    }
+
+    private ResultSummary clone(ResultSummary resultSummary) {
+        SummarizingResultRecorder clone = new SummarizingResultRecorder(resultSummary.getSpecificationDescription());
+        clone.record(resultSummary);
+        return clone;
+    }
+
+    private void addResults(SummarizingResultRecorder accumulator, ResultSummary resultsToAdd) {
         if (resultsToAdd.isForExample()) {
-            recorder.record(new SingleResultSummary(resultsToAdd));
+            accumulator.record(new SingleResultSummary(resultsToAdd));
         } else {
-            recorder.record(resultsToAdd);
+            accumulator.record(resultsToAdd);
         }
-        return recorder;
-    }
-
-    private ConcordionRunOutput getParentFromCacheOrCreate(Fixture fixture, String specificationDescription) {
-        ConcordionRunOutput output = map.get(getID(fixture, null));
-        if (output == null) {
-            specificationDescription = createSummaryDescription(specificationDescription);
-            SummarizingResultRecorder totalActualResults = new SummarizingResultRecorder(specificationDescription);
-            SummarizingResultRecorder totalModifiedResults = new SummarizingResultRecorder(specificationDescription);
-            output = new ConcordionRunOutput(totalActualResults, totalModifiedResults);
-            map.put(getID(fixture, null), output);
-        }
-        return output;
-    }
-
-    private String createSummaryDescription(String specificationDescription) {
-        if (specificationDescription.endsWith(XMLSpecification.OUTER_EXAMPLE_DESCRIPTION_SUFFIX)) {
-            specificationDescription = specificationDescription.substring(0, specificationDescription.length() - XMLSpecification.OUTER_EXAMPLE_DESCRIPTION_SUFFIX.length());
-        }
-        return specificationDescription + " (Summary)";
     }
 
     private ConcordionRunOutput getExampleFromCache(Fixture fixture, String example) {

src/main/java/org/concordion/integration/junit4/ConcordionRunner.java
@@ -9,7 +9,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.concordion.Concordion;
 import org.concordion.api.Fixture;
-import org.concordion.api.Result;
 import org.concordion.api.ResultSummary;
 import org.concordion.internal.*;
 import org.concordion.internal.cache.ConcordionRunOutput;
@@ -148,11 +147,8 @@ public class ConcordionRunner extends BlockJUnit4ClassRunner {
         ConcordionRunOutput results = RunResultsCache.SINGLETON.getFromCache(fixtureClass, null);
 
         if (results != null) {
-            // we only print meta-results when the spec has multiple examples.
-            if (concordionFrameworkMethods.size() > 1) {
-                synchronized (System.out) {
-                    results.getActualResultSummary().print(System.out, setupFixture);
-                }
+            synchronized (System.out) {
+                results.getActualResultSummary().print(System.out, setupFixture);
             }
         }
 

src/main/java/org/concordion/internal/XMLSpecification.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 public class XMLSpecification implements SpecificationByExample {
 
-    private static final String OUTER_DESCRIPTION = "[Outer]";
-    public static final String OUTER_EXAMPLE_DESCRIPTION_SUFFIX = " " + OUTER_DESCRIPTION;
+    public static final String OUTER_EXAMPLE_NAME = "[Outer]";
+    public static final String OUTER_EXAMPLE_SUFFIX = " " + OUTER_EXAMPLE_NAME;
 
     private String testDescription;
 
@@ -82,8 +82,8 @@ public class XMLSpecification implements SpecificationByExample {
     }
 
     public void setFixture(Fixture fixture) {
-        if (examples.size() > 0) {
-            testDescription = OUTER_DESCRIPTION;
+        if (hasExamples()) {
+            testDescription = OUTER_EXAMPLE_NAME;
         } else {
             testDescription = fixture.getSpecificationDescription();
         }
@@ -92,11 +92,6 @@ public class XMLSpecification implements SpecificationByExample {
     public void processExample(Evaluator evaluator, String example, SummarizingResultRecorder resultRecorder) {
         if (testDescription.equals(example)) {
             processNode(rootCommandNode, evaluator, resultRecorder);
-
-            if (examples.size() > 0) {
-                resultRecorder.setSpecificationDescription(resultRecorder.getSpecificationDescription() + OUTER_EXAMPLE_DESCRIPTION_SUFFIX);
-            }
-
             return;
         }
 
@@ -106,6 +101,10 @@ public class XMLSpecification implements SpecificationByExample {
                 processNode(commandCall, evaluator, resultRecorder);
             }
         }
+    }
+
+    private boolean hasExamples() {
+        return examples.size() > 0;
     }
 
     public List<String> getExampleNames() {

commit b211a763d51eaa22b535a671359a0fb8a314e8ec

Fix ClassCastException with ExpectedToFail.
This was caused by the changes in 5cd0cc7cc.
The fix has been tested with serial and parallel runners in all the branches of concordion-scope-examples and gives the same run results in each case.
Fixes #188.

src/main/java/org/concordion/internal/cache/RunResultsCache.java
@@ -128,9 +128,10 @@ public enum RunResultsCache {
             fixtureTotalResults = new ConcordionRunOutput(actualResultSummary, modifiedResultSummary);
             map.put(getID(fixture, null), fixtureTotalResults);
         } else {
-            addResults((SummarizingResultRecorder) fixtureTotalResults.getActualResultSummary(), actualResultSummary);
-            addResults((SummarizingResultRecorder) fixtureTotalResults.getModifiedResultSummary(), modifiedResultSummary);
-        }
+            ResultSummary totalActualResults = addResults(fixtureTotalResults.getActualResultSummary(), actualResultSummary);
+            fixtureTotalResults.setActualResultSummary(totalActualResults);
+            ResultSummary totalModifiedResults = addResults(fixtureTotalResults.getModifiedResultSummary(), modifiedResultSummary);
+            fixtureTotalResults.setModifiedResultSummary(totalModifiedResults);                  }
     }
 
     private ResultSummary clone(ResultSummary resultSummary) {
@@ -139,12 +140,17 @@ public enum RunResultsCache {
         return clone;
     }
 
-    private void addResults(SummarizingResultRecorder accumulator, ResultSummary resultsToAdd) {
-//        if (resultsToAdd.isForExample()) {
-//            accumulator.record(new SingleResultSummary(resultsToAdd));
-//        } else {
-            accumulator.record(resultsToAdd);
-//        }
+
+    private ResultSummary addResults(ResultSummary accumulator, ResultSummary resultsToAdd) {
+        SummarizingResultRecorder recorder;
+        if (accumulator instanceof SummarizingResultRecorder) {
+            recorder = (SummarizingResultRecorder) accumulator;
+        } else {
+            recorder = new SummarizingResultRecorder();
+            recorder.record(accumulator);
+        }
+        recorder.record(resultsToAdd);
+        return recorder;
     }
 
     private ConcordionRunOutput getExampleFromCache(Fixture fixture, String example) {