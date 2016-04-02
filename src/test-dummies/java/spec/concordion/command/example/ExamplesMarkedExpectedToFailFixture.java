c71a7e69b4d3063f1b16b06bf915b96d216b1b7c
Nigel Charman <nigel.charman.nz@gmail.com>
2016-04-03 00:27:35 +1300

    Moved ExpectedToFail and Unimplemented examples to test-dummies.
    This fixes the Concordion spec to be all green again :)
    and adds extra assertions.

---
+++ src/test-dummies/java/spec/concordion/command/example/ExamplesMarkedExpectedToFailFixture.java
@@ -0,0 +1,8 @@
+package spec.concordion.command.example;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class ExamplesMarkedExpectedToFailFixture {
+}
