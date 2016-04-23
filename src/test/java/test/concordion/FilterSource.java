fd8080f4f031bc485b1c0691aceb37d1f31b72f6
Nigel Charman <nigel.charman.nz@gmail.com>
2016-04-23 23:12:35 +1200

    Add fix to FilterSource

--- src/test/java/test/concordion/FilterSource.java
+++ src/test/java/test/concordion/FilterSource.java
@@ -16,10 +16,9 @@ package test.concordion;
 
 import org.concordion.api.Resource;
 import org.concordion.api.Source;
+import org.concordion.internal.ClassPathSource;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class FilterSource implements Source {
 
@@ -39,16 +38,58 @@ public class FilterSource implements Source {
     }
 
     public boolean canFind(Resource resource) {
-        return true;
+        return resource.getName().endsWith(".html");
     }
 
     @Override
     public String readAsString(InputStream inputStream) throws IOException {
-        return null;
+        return readAsString(inputStream, "UTF-8");
     }
 
     @Override
     public String readResourceAsString(String resourcePath) {
-        return null;
+        return readResourceAsString(resourcePath, "UTF-8");
     }
+
+    private String readAsString(InputStream inputStream, String charsetName) throws IOException {
+        Reader reader = new InputStreamReader(inputStream, charsetName);
+        try {
+            return readAsString(reader);
+        } finally {
+            reader.close();
+        }
+    }
+
+    private InputStream getResourceAsStream(String resourcePath) {
+        ClassLoader classLoader = ClassPathSource.class.getClassLoader();
+        return classLoader.getResourceAsStream(resourcePath.replaceFirst("/", ""));
+    }
+
+    private String readAsString(Reader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        while ((line = bufferedReader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private String readResourceAsString(String resourcePath, String charsetName) {
+        try {
+            InputStream in = getResourceAsStream(resourcePath);
+            if (in == null) {
+                throw new IOException("Resource not found");
+            }
+            Reader reader = new InputStreamReader(in, charsetName);
+            try {
+                return readAsString(reader);
+            } finally {
+                reader.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read resource '" + resourcePath + "'", e);
+        }
+    }
+
 }
