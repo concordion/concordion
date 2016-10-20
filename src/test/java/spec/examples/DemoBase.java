6eb076ce0ea314d3ebc81bb90c3c355ba14b7672
Andrew Sumner <andrew.sumner@xtra.co.nz>
2016-10-20 20:26:32 +1300

    Only work with ConcordionResources annotation on declaring class

---
+++ src/test/java/spec/examples/DemoBase.java
@@ -0,0 +1,11 @@
+package spec.examples;
+
+import org.concordion.api.ConcordionResources;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+@ConcordionResources(value="/concordion.css", includeDefaultStyling = false)
+public class DemoBase {
+
+}
--- src/test/java/spec/examples/DemoTest.java
+++ src/test/java/spec/examples/DemoTest.java
@@ -1,11 +1,6 @@
 package spec.examples;
 
-import org.concordion.integration.junit4.ConcordionRunner;
-import org.concordion.internal.util.SimpleFormatter;
-import org.junit.runner.RunWith;
-
-@RunWith(ConcordionRunner.class)
-public class DemoTest {
+public class DemoTest extends DemoBase {
 
     public String greetingFor(String firstName) {
         return String.format("Hello %s!", firstName);
