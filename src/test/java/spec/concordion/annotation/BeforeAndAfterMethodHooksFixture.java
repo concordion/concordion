56bc85292af2b066028f1496f5fcd70fff33aee2
Nigel Charman <nigel.charman.nz@gmail.com>
2016-10-24 12:21:08 +1300

    Improved Before and After method hooks spec. Now includes a before example to lock in behaviour of before method hooks on before example. Also has an exception and failing test case.

--- src/test/java/spec/concordion/annotation/BeforeAndAfterMethodHooksFixture.java
+++ src/test/java/spec/concordion/annotation/BeforeAndAfterMethodHooksFixture.java
@@ -1,13 +1,26 @@
 package spec.concordion.annotation;
 
+import java.io.File;
+import java.lang.reflect.TypeVariable;
 import java.util.List;
+import java.util.Map;
 
+import org.concordion.api.CommandCall;
+import org.concordion.api.Element;
+import org.concordion.api.Resource;
 import org.concordion.integration.junit4.ConcordionRunner;
+import org.concordion.internal.*;
+import org.concordion.internal.command.RunCommand;
 import org.junit.runner.RunWith;
+import test.concordion.RunCommandSimulator;
 
 @RunWith(ConcordionRunner.class)
 public class BeforeAndAfterMethodHooksFixture {
     public List<String> getLog() {
         return BeforeAndAfterLoggingFixture.getLog();
+    }
+
+    public void simulateRun(final String href) throws Exception {
+        new RunCommandSimulator().simulate(href, this.getClass());
     }
 }
