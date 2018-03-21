c63d6a7baeaa12166d38aba4b6eafc4eb4dfbe67
Bartosz Koperski <b_koperski@outlook.com>
2018-03-21 08:45:05 +0100

    Review

--- src/test-dummies/java/spec/concordion/common/command/example/FailFastFixture.java
+++ src/test-dummies/java/spec/concordion/common/command/example/FailFastFixture.java
@@ -1,13 +1,11 @@
 package spec.concordion.common.command.example;
 
-import org.concordion.api.ExpectedToFail;
 import org.concordion.api.FailFast;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
 @RunWith(ConcordionRunner.class)
 @FailFast
-@ExpectedToFail
 public class FailFastFixture {
 
     public void throwException() throws Exception {
