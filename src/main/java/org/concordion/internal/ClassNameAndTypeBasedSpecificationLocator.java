be0919fa0632599b448e4948c22c2e9b445d00c5
Nigel Charman <nigel.charman.nz@gmail.com>
2018-06-23 22:36:49 +1200

    Remove unused class

--- src/main/java/org/concordion/internal/ClassNameAndTypeBasedSpecificationLocator.java
+++
@@ -1,7 +0,0 @@
-package org.concordion.internal;
-
-import org.concordion.api.Resource;
-
-public abstract class ClassNameAndTypeBasedSpecificationLocator extends ClassNameBasedSpecificationLocator {
-    public abstract Resource locateSpecification(FixtureType fixtureType, String typeSuffix);
-}
