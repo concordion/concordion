4093b812e7ba7afafa3e8427f98f662812d0e42a
Nigel Charman <nigel.charman.nz@gmail.com>
2018-07-01 17:31:46 +1200

    Added deprecated methods for backward compatibility

--- src/main/java/org/concordion/internal/ClassNameBasedSpecificationLocator.java
+++ src/main/java/org/concordion/internal/ClassNameBasedSpecificationLocator.java
@@ -5,9 +5,17 @@ import org.concordion.api.SpecificationLocator;
 import org.concordion.internal.util.Check;
 
 public class ClassNameBasedSpecificationLocator implements SpecificationLocator {
+
+    public ClassNameBasedSpecificationLocator() {
+    }
+
+    @Deprecated
+    public ClassNameBasedSpecificationLocator(String specificationSuffix) {
+    }
+
     @Override
-    public Resource locateSpecification(FixtureType fixtureType, String typeSuffix) {
+    public Resource locateSpecification(FixtureType fixtureType, String specificationSuffix) {
         Check.notNull(fixtureType, "Fixture is null");
-        return FixtureSpecificationMapper.toSpecificationResource(fixtureType, typeSuffix);
+        return FixtureSpecificationMapper.toSpecificationResource(fixtureType, specificationSuffix);
     }
 }
