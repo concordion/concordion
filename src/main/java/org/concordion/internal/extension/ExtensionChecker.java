0dc478448569537762a80f707de0fee70ed1e71b
Nigel Charman <nigel.charman.nz@gmail.com>
2018-07-01 17:33:01 +1200

    Updated ExtensionChecker to check for compatibility with Concordion 2.2.0

--- src/main/java/org/concordion/internal/extension/ExtensionChecker.java
+++ src/main/java/org/concordion/internal/extension/ExtensionChecker.java
@@ -8,10 +8,13 @@ public class ExtensionChecker {
     private static final String MARKDOWN_EXTENSION_CLASS = "org.concordion.ext.MarkdownExtension";
     private static final String EXTENSIONS_CLASS = "org.concordion.ext.Extensions";
     private static final String EXCEL_EXTENSION_SOURCE_CLASS = "org.concordion.ext.excel.ExcelClassPathSource";
+    private static final String RUN_TOTALS_EXTENSION_CLASS = "org.concordion.ext.runtotals.RunTotalsExtension";
+    private static final String EXECUTE_ONLY_IF_EXTENSION_CLASS = "org.concordion.ext.ExecuteOnlyIfExtension";
 
     public static void checkForOutdatedExtensions() {
         checkForExtensionsNowInCore();
         checkForBreakingChanges();
+        checkCompatibility();
         checkForDeprecatedExtensions();
     }
 
@@ -31,8 +34,32 @@ public class ExtensionChecker {
         } catch (ClassNotFoundException expected) {
             return; // We don't want it to be found
         }
-        String msg = "The Concordion Excel Extension must be updated to the latest version to work with Concordion 2.0 or later.";
+        String msg = "The Concordion Excel Extension must be updated to the latest version to work with Concordion 2.0.0 or later.";
         throw new ConfigurationException(msg);
+    }
+
+    private static void checkCompatibility() {
+        assertCompatibleVersion(RUN_TOTALS_EXTENSION_CLASS, "concordion-run-totals-extension", "1.2", "2.2.0");
+        assertCompatibleVersion(EXECUTE_ONLY_IF_EXTENSION_CLASS, "execute-executeonlyif-extension", "0.3", "2.2.0");
+    }
+
+    private static void assertCompatibleVersion(String extensionClassName, String extensionName, String requiredSpecificationVersion, String breakingConcordionVersion) {
+        try {
+            Class<?> extensionClass = Class.forName(extensionClassName);
+            Package extensionPackage = extensionClass.getPackage();
+            String specificationVersion = extensionPackage.getSpecificationVersion();
+            if (extensionPackage.getImplementationTitle() != null) {
+                extensionName = extensionPackage.getImplementationTitle();
+            }
+            if (specificationVersion == null || !extensionPackage.isCompatibleWith(requiredSpecificationVersion)) {
+                String msg = "The " + extensionName + " must be updated to version " + requiredSpecificationVersion +
+                        ".0 or later to work with Concordion " + breakingConcordionVersion + " or later.";
+                throw new ConfigurationException(msg);
+            }
+            return; // Passes the check since it's the right version
+        } catch (ClassNotFoundException expected) {
+            return; // Passes the check since it's not on classpath
+        }
     }
 
     private static void checkForDeprecatedExtensions() {
