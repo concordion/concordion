4b758852bd836d218e703e8ffb94f2de04ca4ae5
Nigel Charman <nigel.charman.nz@gmail.com>
2023-05-28 11:49:06 +1200

    Clean up deprecated methods

--- src/test/java/spec/concordion/common/command/assertFalse/AssertFalseTest.java
+++ src/test/java/spec/concordion/common/command/assertFalse/AssertFalseTest.java
@@ -10,7 +10,7 @@ public class AssertFalseTest {
     
     public String successOrFailure(String fragment, String evaluationResult) {
         return new TestRig()
-            .withStubbedEvaluationResult(new Boolean(evaluationResult))
+            .withStubbedEvaluationResult(Boolean.valueOf(evaluationResult))
             .processFragment(fragment)
             .successOrFailureInWords();
     }
